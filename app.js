f8044a8709f3de1e2a406e5b09dd0783c48667ef
Ugo-X <ugonnadike001@gmail.com>
2024-04-21 17:48:17 +0100

    final commit

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const productsContainer = document.querySelector(".products-container");
 
 const displayProducts = () => {
   // if statement
+  if (filteredProducts.length < 1) {
+    productsContainer.innerHTML = `<h5>Sorry no products matched your search </h5>`;
+    return;
+  }
 
   productsContainer.innerHTML = filteredProducts
     .map(({ id, title, image, price }) => {
@@ -35,7 +39,37 @@ form.addEventListener("keyup", () => {
   filteredProducts = products.filter((product) => {
     return product.title.includes(inputValue);
   });
-  displayProducts()
+  displayProducts();
 });
 
+// Filtering based on compnay btns
+const companies = document.querySelector(".companies");
 
+const displayButtons = () => {
+  const uniqueButtons = [
+    ...new Set(products.map((product) => product.company)),
+  ];
+  companies.innerHTML = uniqueButtons
+    .map((company) => {
+      return `  <button class="company-btn btn" data-id = "${company}">${company}</button>`;
+    })
+    .join("");
+  console.log(uniqueButtons);
+};
+
+displayButtons();
+
+companies.addEventListener("click", (evt) => {
+  const el = evt.target;
+  if (el.classList.contains("company-btn")) {
+    if (el.dataset.id === "all") {
+      filteredProducts = [...products];
+    } else {
+      filteredProducts = products.filter((product) => {
+        return product.company === el.dataset.id;
+      });
+    }
+    searchInput.value = ''
+    displayProducts()
+  }
+});
