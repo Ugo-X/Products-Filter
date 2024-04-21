e0c133712f22a1d797f75ce0286839443b04f272
Ugo-X <ugonnadike001@gmail.com>
2024-04-21 03:40:26 +0100

    filtered based on text

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-console.log('filters project');
+let filteredProducts = [...products];
+
+const productsContainer = document.querySelector(".products-container");
+
+const displayProducts = () => {
+  // if statement
+
+  productsContainer.innerHTML = filteredProducts
+    .map(({ id, title, image, price }) => {
+      return ` <!-- single product -->
+<article class="product" data-id = "${id}">
+  <img src="${image}" alt="product image" class="product-img img">
+  <!-- footer -->
+  <footer>
+    <h5 class="product-name">
+${title}
+    </h5>
+    <span class="product-price">$${price}</span>
+  </footer>
+</article>
+<!-- end of single product -->
+`;
+    })
+    .join("");
+};
+displayProducts();
+
+// # Filter Based on Text
+// select elements
+const form = document.querySelector(".input-form");
+const searchInput = document.querySelector(".search-input");
+
+form.addEventListener("keyup", () => {
+  const inputValue = searchInput.value.toLowerCase();
+  filteredProducts = products.filter((product) => {
+    return product.title.includes(inputValue);
+  });
+  displayProducts()
+});
+
+
